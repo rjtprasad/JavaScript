c4b3e6245950503678c595effae49162d72dc698
RAJAT PRASAD <54785312+rjtprasad@users.noreply.github.com>
2024-03-31 18:36:06 +0000

    Learn variables

---
+++ basics_01/variables.js
@@ -0,0 +1,8 @@
+const accountId = 9960
+let accountEmail = "prasadrajat28@gmail.com"
+var accountPassword = "12345678"
+accountState = "Jamshedpur"
+let accountCity
+
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
